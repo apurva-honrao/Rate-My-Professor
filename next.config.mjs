77ceb572b67592b4ddd7b4b0ee21b0d582bbd487
AbduHany <abdu.hany@gmail.com>
2024-08-27 10:43:33 +0300

    Modify next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // (Optional) Export as a static site
-    // See https://nextjs.org/docs/pages/building-your-application/deploying/static-exports#configuration
-    output: 'export', // Feel free to modify/remove this option
-
     // Override the default webpack configuration
     experimental: {
         serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
